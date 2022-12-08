87b594a24670950bc042d8018df5d65ce2258234
Stephen Isoboye <stephenjasper38@gmail.com>
2022-12-08 15:57:31 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
- * main: Prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
+ * main- Entry point
+ *
+ * Description: DIsplay something
  * Return: Always 0.
  */
 int main(void)
