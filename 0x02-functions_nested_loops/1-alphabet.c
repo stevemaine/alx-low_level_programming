5fd6c1dbe853e483bf5fbca339daac25532fe710
Stephen Isoboye <stephenjasper38@gmail.com>
2022-12-13 13:20:31 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,8 @@
 int main(void)
 {
 	char alphabet;
-	for (alphabet ='a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 	_putchar('\n');
 }
