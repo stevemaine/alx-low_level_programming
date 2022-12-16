a6efdbea952457618d07ea480c3626e17516c0d4
Stephen Isoboye <stephenjasper38@gmail.com>
2022-12-15 18:05:26 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
- * Return: void
+ * print_most_numbers - Prints numer between 0 and 9 with new line
+ * Return:void
  *
  */
-
 void print_most_numbers(void)
 {
 	char c;
+
 
 	for (c = '0'; c <= '9'; c++)
 	{
