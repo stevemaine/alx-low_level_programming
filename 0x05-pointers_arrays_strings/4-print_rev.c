348dc90df1c145c6bd1b2eafde25ce4d4777417b
Stephen Isoboye <stephenjasper38@gmail.com>
2022-12-20 05:52:04 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ *
+ * Return: no return.
+ */
+void print_rev(char *s)
+{
+	int m = 0;
+
+	while (m >= 0)
+	{
+		if (s[m] == '\0')
+			break;
+		m++;
+	}
