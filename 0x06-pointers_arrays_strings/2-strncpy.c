f97a62c9dadb14ac5eee8c051504636651c5d403
Stephen Isoboye <stephenjasper38@gmail.com>
2022-12-21 09:28:28 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,6 +6,7 @@
  * @src: The source of strings
  * @dest: The destination of the string
  * @n: The length of int
+ * stevemaine
  * Return: pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
