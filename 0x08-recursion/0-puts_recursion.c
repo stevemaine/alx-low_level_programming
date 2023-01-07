51d0610baeccd25d20c1dc4fbd72a57ef7f02c7e
Stephen Isoboye <stephenjasper38@gmail.com>
2023-01-06 18:02:22 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') // base case: if the current character is the null terminator, we have reached the end of the string
-    {
-        printf("\n"); // print a new line
-        return; // terminate the function
-    }
-
-    printf("%c", *s); // print the current character
-    _puts_recursion(s + 1); // call the function with the next character in the string
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
 }
